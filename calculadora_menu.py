132c6b57d93a6c3d41847eafca6491a733685988
ivanRamiro05 <ivan2224654@correo.uis.edu.co>
2022-09-10 08:54:30 -0500

    actualizacion del codigo de python

--- calculadora_menu.py
+++ calculadora_menu.py
@@ -7,13 +7,51 @@ print("-----------------------------")
 
 #input
 bandera=False
-X=float(input("Dame el valor  del número x: "))
-Y=float(input("Dame el valor  del número y: "))
+x=float(input("Dame el valor  del número x: "))
+y=float(input("Dame el valor  del número y: "))
 
 print("\n dame la opción que deseas realizar: \n")
 #se despliega el menu para seleccionar la opcion que deseas realizar
 print("1.Sumar(el primero mas el segundo)")
 print("2.Restar(el primero menos el segundo)")
 print("3.Multiplicar(el primero por el segundo)")
-print("4.Potenciar(el primero a la potencia  del segundo)")
-print("5.Logaritmo(de el primero entre el segundo)")
+print("4.dividir(el primero entre el segundo)")
+print("5.Potenciar(el primero a la potencia  del segundo)")
+print("6.Logaritmo(de el primero ")
+
+#PROCESSING
+opcion=int(input("\n Dame la opción: "))
+if(opcion==1):
+    z= x + y
+    print(x,"+",y)
+elif (opcion==2):
+    z = x - y
+    print(x,"-",y)
+elif(opcion==3):
+    z = x * y
+    print(x,"X",y)
+elif (opcion==4 and y!=0):
+    z = x / y
+    print(x,"/",y)
+elif(opcion==4 and y==0):
+    print("El denominador es igial a cero y")
+    print("NO se puede realizar la division.")
+    bandera= True
+elif (opcion==5):
+    z = pow(x,y)
+    print(x,"^",y)
+elif(opcion==6 and x>0):
+    z=log(x)
+    print("Logaritmo de",x)
+elif(opcion==6 and x<=0):
+    print("El valor de x es <= a cero y")
+    print("NO se puede calcular el logaritmo. ")
+else:
+    print("opcion no valida ")
+
+    # se escribe el resultado con otra condicion 
+if(opcion<7 and bandera==False):
+        print("Resultado =",z)
+
+#fin
+
